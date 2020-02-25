ec7642de884dda518793324b692e9eaffe59003e
Frederik Mortensen <frederik.mortensen@internetx.com>
2020-02-25 18:31:55 +0100

    wip

--- halfbroke/src/main/java/de/javanachhilfe/halfbroke/connectivity/ConnectionManager.java
+++ halfbroke/src/main/java/de/javanachhilfe/halfbroke/connectivity/ConnectionManager.java
@@ -2,7 +2,6 @@ package de.javanachhilfe.halfbroke.connectivity;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 /**
  * 
@@ -11,31 +10,76 @@ import java.sql.PreparedStatement;
  */
 public class ConnectionManager {
 
+	private static ConnectionManager connectionManager = null;
+
+	private Connection connection = null;
+
+	//CREATE USER 'fred'@'localhost' IDENTIFIED BY 'test';
+	//GRANT ALL PRIVILEGES ON halfbroke.* TO 'fred'@'localhost';
+	//DROP USER 'fred'@'localhost';
+	//private static final String SQL_CREATE_TABLE = "CREATE TABLE `halfbroke`.`test` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `testcolumn` VARCHAR, PRIMARY KEY (`id`))";
+
 	/**
 	 * 
 	 */
-	public ConnectionManager() {
+	private ConnectionManager() {
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static ConnectionManager getInstance() throws Exception {
+		if(connectionManager == null) {
+			connectionManager = new ConnectionManager();
+		}
+		return connectionManager;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public void connect() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		String url = "jdbc:mysql://localhost:3306/halfbroke";
+		String username = "fred";
+		String password = "test";
+
+		connection = DriverManager.getConnection(url, username, password);
+	}
+	
+	/**
+	 * 
+	 */
+	public void disconnect() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			String url = "jdbc:mysql://localhost:3306/halfbroke";
-			String username = "root";
-			String password = "root";
-
-			Connection connection = DriverManager.getConnection(url, username, password);
-
-			PreparedStatement stmt = connection.prepareStatement("insert into Emp values(?,?)");
-			stmt.setInt(1, 101);// 1 specifies the first parameter in the query
-			stmt.setString(2, "Ratan");
-
-			int i = stmt.executeUpdate();
-			System.out.println(i + " records inserted");
-
+			connection.rollback();
 			connection.close();
-
-		} catch (Exception e) {
-			System.out.println(e);
+		} catch(Exception e) {
+			connection = null;
 		}
 	}
+
+	/**
+	 * 
+	 * @param connection
+	 */
+//	public void creatTable(String sql) {
+//		try {
+//			PreparedStatement stmt = connection.prepareStatement(SQL_CREATE_TABLE);
+//			// stmt.setInt(1, 101);// 1 specifies the first parameter in the query
+//			// stmt.setString(2, "Ratan");
+//
+//			int i = stmt.executeUpdate();
+//			System.out.println(i + " records inserted");
+//
+//			connection.close();
+//
+//		} catch(Exception e) {
+//			System.out.println(e);
+//		}
+//	}
 
 }
--- halfbroke/src/main/java/de/javanachhilfe/halfbroke/persistence/EntityManager.java
+++ halfbroke/src/main/java/de/javanachhilfe/halfbroke/persistence/EntityManager.java
@@ -1,7 +1,9 @@
 package de.javanachhilfe.halfbroke.persistence;
 
-import java.sql.ConnectionBuilder;
-import java.sql.PreparedStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.javanachhilfe.halfbroke.connectivity.ConnectionManager;
 
 /**
  * 
@@ -9,17 +11,63 @@ import java.sql.PreparedStatement;
  *
  */
 public class EntityManager<T> {
-	
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	private static EntityManager entityManager = null;
+
+	private ConnectionManager connectionManager = null;
+
+	/**
+	 * 
+	 */
+	private EntityManager() {
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static EntityManager getInstance() throws Exception {
+		if(entityManager == null) {
+			entityManager = new EntityManager();
+			entityManager.setConnectionManager(ConnectionManager.getInstance());
+		}
+		return entityManager;
+	}
+
+	/**
+	 * 
+	 */
+	public void test() throws Exception {
+
+		logger.info("Connecting...");
+
+		connectionManager.connect();
+		
+		Thread.sleep(1000);
+		
+		connectionManager.disconnect();
+	}
+
 	public T read(T entity) {
-		Connection connection = ConnectionManager.getConnection();
-		PreparedStatement preparedStatement;
-		PreparedStatement.
+		// PreparedStatement preparedStatement;
+		// PreparedStatement.
 		return null;
 	}
-	
+
 	public boolean write(T entity) {
-		
 		return false;
+	}
+
+	public void setConnectionManager(ConnectionManager connectionManager) {
+		this.connectionManager = connectionManager;
+	}
+
+	public ConnectionManager getConnectionManager() {
+		return connectionManager;
 	}
 
 }
