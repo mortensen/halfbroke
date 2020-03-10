e3a099985e3bf3b578fbcdee94d1c69e7afd6f1b
Frederik Mortensen <frederik.mortensen@internetx.com>
2020-03-10 18:12:21 +0100

    wip

--- halfbroke/src/main/resources/grants.sql
+++ halfbroke/src/main/resources/grants.sql
@@ -1,5 +1,8 @@
-CREATE USER 'fred'@'localhost' IDENTIFIED BY 'test';
-GRANT ALL PRIVILEGES ON halfbroke.* TO 'fred'@'localhost';
-CREATE TABLE `halfbroke`.`Person` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `firstName` VARCHAR(50), `lastName` VARCHAR(50), PRIMARY KEY (`id`) );
+#drop database halfbroke;
+#drop user 'duke'@'localhost';
+#flush privileges;
 
-#DROP USER 'fred'@'localhost';
+CREATE DATABASE `halfbroke`;
+CREATE USER 'duke'@'localhost' IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON halfbroke.* TO 'duke'@'localhost';
+CREATE TABLE `halfbroke`.`Person` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `firstName` VARCHAR(50), `lastName` VARCHAR(50), PRIMARY KEY (`id`) );
