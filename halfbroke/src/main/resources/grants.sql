8bda24b31d6aaba78d0573a2ab0d07c7ed319d84
Frederik Mortensen <frederik.mortensen@internetx.com>
2020-03-10 19:31:31 +0100

    wip

--- halfbroke/src/main/resources/grants.sql
+++
@@ -1,8 +0,0 @@
-#drop database halfbroke;
-#drop user 'duke'@'localhost';
-#flush privileges;
-
-CREATE DATABASE `halfbroke`;
-CREATE USER 'duke'@'localhost' IDENTIFIED BY 'test';
-GRANT ALL PRIVILEGES ON halfbroke.* TO 'duke'@'localhost';
-CREATE TABLE `halfbroke`.`Person` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `firstName` VARCHAR(50), `lastName` VARCHAR(50), PRIMARY KEY (`id`) );
---
+++ halfbroke/src/main/resources/sql_preparations.sql
@@ -0,0 +1,9 @@
+#drop database halfbroke;
+#drop user 'duke'@'localhost';
+#flush privileges;
+
+CREATE DATABASE `halfbroke`;
+CREATE USER 'duke'@'localhost' IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON halfbroke.* TO 'duke'@'localhost';
+CREATE TABLE `halfbroke`.`Person` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `firstName` VARCHAR(50), `lastName` VARCHAR(50), PRIMARY KEY (`id`) );
+INSERT INTO `halfbroke`.`Person` (`id`, `firstName`, `lastName`) VALUES (1, 'Duke', 'Masters');
